82934b2174ea52f1ceeac3af9df4fe150600bb1f
MatheusCandidoo <oliveiracandidomatheus@gmail.com>
2022-12-13 20:28:30 -0300

    Separação de responsabilidades

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
-
-var mensagem = prompt('Digite a mensagem a ser enviada', '');
-var repeticoes = parseInt(prompt('Digite o numero de repetições', 10));
-
-async function enviarMensagems(mensagem, repeticoes) {
+function identifyTextArea() {
     main = document.querySelector("#main"),
         textarea = main.querySelector(`div[contenteditable="true"]`)
 
-    if (!textarea) throw new Error("Não há uma conversa aberta")
-
-
-    for (var i = 0; i < repeticoes; i++) {
-        var line = mensagem + ' ' + (i+1)
-        console.log(line)
-
-        textarea.focus();
-        document.execCommand('insertText', false, line);
-        textarea.dispatchEvent(new Event('change', { bubbles: true }));
-
-        setTimeout(() => {
-            (main.querySelector(`[data-testid="send"]`) || main.querySelector(`[data-icon="send"]`)).click();
-        }, 100);
-        if (i !== repeticoes)
-            await new Promise(resolve => setTimeout(resolve, 250));
-    }
-
-
-    return repeticoes;
+    return textarea;
 }
 
-await enviarMensagems(mensagem, repeticoes).then(e => console.log(`Código finalizado, ${e} mensagens enviadas`)).catch(console.error);
+function textareaDontExists() {
+    if (!textarea) throw new Error("Não há uma conversa aberta")
+}
+
+async function sendMessages(message, counter) {
+    textarea = identifyTextArea();
+
+    textareaDontExists();
+
+    for (var i = 0; i < counter; i++) {
+
+        setMessagOnTextarea(`${message} ${i + 1}`);
+
+        clickSendButton()
+
+        if (i !== counter)
+            await new Promise(resolve => setTimeout(resolve, 250));
+    }
+    return counter;
+}
+
+function clickSendButton() {
+    setTimeout(() => {
+        (main.querySelector(`[data-testid="send"]`) || main.querySelector(`[data-icon="send"]`)).click();
+    }, 100);
+}
+
+function setMessagOnTextarea(line) {
+    console.log(line)
+
+    textarea.focus();
+    document.execCommand('insertText', false, line);
+    textarea.dispatchEvent(new Event('change', { bubbles: true }));
+}
+
+function start() {
+    var message = prompt('Digite a mensagem a ser enviada', '');
+    var counter = parseInt(prompt('Digite o numero de repetições', 10));
+
+    enviarMensagems(message, counter).then(e => console.log(`Código finalizado, ${e} mensagens enviadas`)).catch(console.error);
+}
+
+start();
